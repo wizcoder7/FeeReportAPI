a9475d921c0787e326c1d6810532d72f1934f99d
wizcoder7 <97383401+wizcoder7@users.noreply.github.com>
2022-01-30 23:09:34 +0530

    Added the Java Classes

---
+++ src/com/java/feereport/Accountant.java
@@ -0,0 +1,16 @@
+package com.javatpoint.feereport;
+
+public class Accountant {
+private int id;
+private String name,password,email,contactno;
+public Accountant() {}
+public Accountant(String name, String password, String email, String contactno) {
+	super();
+	this.name = name;
+	this.password = password;
+	this.email = email;
+	this.contactno = contactno;
+}
+
+
+}
